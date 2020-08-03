eb0cee377ce7a7ca19fe7fbcef28b37a79dc0623
sparmar <sparmar@nextfaze.com>
2020-08-04 08:15:05 +0930

    Add url launcher imports

--- lib/manup.dart
+++ lib/manup.dart
@@ -1,10 +1,13 @@
 library manup;
 
+import 'package:manup/src/ui/manup_app_dialog.dart';
+import 'package:manup/src/manup_delegate.dart';
 import 'package:package_info/package_info.dart';
 import 'package:pub_semver/pub_semver.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
 import 'package:meta/meta.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 part 'src/exception.dart';
 part 'src/metadata.dart';
