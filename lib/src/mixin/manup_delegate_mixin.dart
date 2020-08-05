02ed31dd5fb7157e596500bc965dc42dcd6459d1
sparmar <sparmar@nextfaze.com>
2020-08-05 15:21:15 +0930

    Remove shouldShowManupAlert methods

--- lib/src/mixin/manup_delegate_mixin.dart
+++ lib/src/mixin/manup_delegate_mixin.dart
@@ -2,8 +2,6 @@ part of manup;
 
 /// Default implemetation of [ManupDelegate]
 mixin ManupDelegateMixin on ManupDelegate {
-  bool get shouldShowManupAlert => true;
-  // informative
   void manupStatusChanged(ManUpStatus status) {}
   void manUpConfigUpdateStarting() {}
   void manUpUpdateRequired() {}
