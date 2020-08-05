22bc64b77ee2aa1ea3a7cf6ca120fcca788faff8
sparmar <sparmar@nextfaze.com>
2020-08-05 11:03:02 +0930

    Update manup app dialog

--- lib/src/ui/manup_app_dialog.dart
+++ lib/src/ui/manup_app_dialog.dart
@@ -1,50 +1,28 @@
 part of manup;
 
 class ManupAppDialog {
-  Future<bool> showConfirmDialog(
+  Future<bool> showAlertDialog(
       {String message = " ",
       String trueText = "ok",
-      String falseText = "cancel",
+      String falseText,
       bool barrierDismissible = false,
       @required BuildContext context}) {
+    bool hasCancelText = falseText != null || falseText.isNotEmpty;
     return showDialog(
         barrierDismissible: barrierDismissible,
         context: context,
         builder: (BuildContext context) {
           return WillPopScope(
-            onWillPop: () => Future.value(false),
+            onWillPop: () => Future.value(barrierDismissible),
             child: AlertDialog(
               title: Text(message),
               actions: <Widget>[
-                FlatButton(
-                  child: Text(falseText),
-                  onPressed: () => Navigator.of(context).pop(false),
-                ),
-                FlatButton(
-                  child: Text(trueText,
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                  onPressed: () => Navigator.of(context).pop(true),
-                )
-              ],
-            ),
-          );
-        });
-  }
-
-  Future<bool> showErrorDialog(
-      {String message = " ",
-      String trueText = "ok",
-      bool barrierDismissible = false,
-      @required BuildContext context}) {
-    return showDialog(
-        barrierDismissible: barrierDismissible,
-        context: context,
-        builder: (BuildContext context) {
-          return WillPopScope(
-            onWillPop: () => Future.value(false),
-            child: AlertDialog(
-              title: Text(message),
-              actions: <Widget>[
+                hasCancelText
+                    ? FlatButton(
+                        child: Text(falseText),
+                        onPressed: () => Navigator.of(context).pop(false),
+                      )
+                    : Container(),
                 FlatButton(
                   child: Text(trueText,
                       style: TextStyle(fontWeight: FontWeight.bold)),
