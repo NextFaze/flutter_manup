7b2b541899dbc4a713c7e39ca489f7c4ed32afec
sparmar <sparmar@nextfaze.com>
2020-08-05 16:27:22 +0930

    Fix mixin delegate typo

--- lib/src/mixin/manup_delegate_mixin.dart
+++ lib/src/mixin/manup_delegate_mixin.dart
@@ -2,7 +2,7 @@ part of manup;
 
 /// Default implemetation of [ManupDelegate]
 mixin ManupDelegateMixin on ManupDelegate {
-  void manupStatusChanged(ManUpStatus status) {}
+  void manUpStatusChanged(ManUpStatus status) {}
   void manUpConfigUpdateStarting() {}
   void manUpUpdateRequired() {}
   void manUpUpdateAvailable() {}
