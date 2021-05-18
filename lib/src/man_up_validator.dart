71c6a6541a851431b081d39579330dde683e0feb
Shyam Parmar <it-sam@users.noreply.github.com>
2021-05-18 16:30:32 +0930

    Feature/seperate validator (#17)
    * Seperate man up validate function.
    
    * Add man up validator file as part of ManUp
    
    * Update readme and bump the version.
    
    Co-authored-by: sparmar <sparmar@nextfaze.com>

---
+++ lib/src/man_up_validator.dart
@@ -0,0 +1,26 @@
+part of manup;
+
+///
+Future<ManUpStatus> validatePlatformData(
+    {required String version, required PlatformData platformData}) async {
+  //
+  if (!platformData.enabled) {
+    return ManUpStatus.disabled;
+  }
+
+  try {
+    Version currentVersion = Version.parse(version);
+    VersionConstraint latestVersion =
+        VersionConstraint.parse('>=${platformData.latestVersion}');
+    VersionConstraint minVersion =
+        VersionConstraint.parse('>=${platformData.minVersion}');
+    if (latestVersion.allows(currentVersion)) {
+      return ManUpStatus.latest;
+    } else if (minVersion.allows(currentVersion)) {
+      return ManUpStatus.supported;
+    }
+    return ManUpStatus.unsupported;
+  } catch (exception) {
+    throw ManUpException(exception.toString());
+  }
+}
